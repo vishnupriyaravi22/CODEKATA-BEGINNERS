16c503ca0cd0166fd1f215ccffe496ba2f2310d7
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-02-06 07:02:18 -0800

    Create 33_count_space.c

---
+++ BEG_SET4/33_count_space.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int count=0;
+    char str[100];
+    scanf(" %[^\n]", str);
+    for (int i = 0;str[i] != '\0';i++)
+    {
+        if (str[i] == ' ')
+            count++;    
+    }
+    printf("%d\n", count);
+
+    return 0;
+}
