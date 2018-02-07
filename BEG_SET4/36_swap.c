8f0d8546fd7c7c323694a009b5b93f78938d350f
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-02-07 08:41:43 -0800

    Create 36_swap.c

---
+++ BEG_SET4/36_swap.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,temp;
+    scanf("%d%d",&a,&b);
+    if(temp==0)
+    temp =a;
+    a=b;
+    b=temp;
+    printf("%d %d",a,b);
+
+
+   return 0; 
+}
