9ad0f110be72ed043946ab740d6060c0dd618367
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-02-12 15:22:23 +0530

    Create 41_print k times.c

---
+++ SET5/41_print k times.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+   int n;
+   char c[100];
+  
+   scanf("%s",c);
+   scanf("%d",&n);
+   //printf("%s %d",c,n);
+   for(int i=0;i<n;i++)
+   {
+       printf("\n%s",c);
+   }
+   return 0;
+   
+}
