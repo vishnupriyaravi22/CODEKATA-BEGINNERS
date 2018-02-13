2f1bf7db917fae2bae826ee59fdee7862e6665f1
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-02-13 07:47:54 -0800

    Create 45-next-num.c

---
+++ SET5/45-next-num.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    long long n;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+
+        n=n+1;
+    
+    printf("Next digits: %d", n);
+}
