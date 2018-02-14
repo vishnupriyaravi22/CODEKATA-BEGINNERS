c6669e346666ec9d75d04147fa48a21500ddc652
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-02-14 08:50:33 -0800

    Create 49-sum-(posted by arun prakesh).c

---
+++ SET5/49-sum-(posted by arun prakesh).c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2,sum;
+    scanf("%d %d",&n1,&n2);
+    sum = n1+n2;
+    printf("%d-(Posted by Arun prakash)",sum);
+    
+ 
+    
+    return 0;
+}
