0fa13450e39e5aa62e62c4b718280c17f86c59ac
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-02-23 09:56:56 +0530

    Update 45_count-num.c

--- SET5/45_count-num.c
+++ SET5/45_count-num.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    int n;
-    int count = 0;
+    long long n;
+    int c = 0;
 
     printf("Enter an integer: ");
-    scanf("%d", &n);
+    scanf("%lld", &n);
 
     while(n != 0)
     {
         // n = n/10
         n /= 10;
-        ++count;
+        ++c;
     }
 
-    printf("Number of digits: %d", count);
+    printf("%d", c);
 }
+
