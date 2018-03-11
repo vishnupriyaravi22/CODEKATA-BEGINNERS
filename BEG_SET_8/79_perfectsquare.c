8d6830dc2355309314843e4d475093a8ee8c6687
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-11 01:06:16 -0800

    Update 79_perfectsquare.c

--- BEG_SET_8/79_perfectsquare.c
+++ BEG_SET_8/79_perfectsquare.c
@@ -1,12 +1,32 @@
 #include<stdio.h>
 int main()
 {
-int n1,n2,pro;
+int n1,n2,pro,flag=0;
 scanf("%d %d",&n1,&n2);
 pro=n1*n2;
-if(n1==n2)
-    printf("yes");
-    else
-    printf("no");
-return 0;
+ for(int i=1;i<=pro/2;i++)
+	{
+		if
+
+(i*i==pro)
+		{
+			
+
+flag=1;
+			
+
+break;
+		}
+	
+	}
+	
+	if(flag==1)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
 }
