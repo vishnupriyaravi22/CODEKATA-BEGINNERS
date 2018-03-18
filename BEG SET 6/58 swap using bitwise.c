022d81d58172f3cf9e3000ea7aceab08663b5812
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-18 10:10:01 -0700

    Create 58 swap using bitwise.c

---
+++ BEG SET 6/58 swap using bitwise.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+   int x,y;
+   scanf("%d %d",&x,&y);
+   if(a!=0)
+   {
+       x=x^y;
+      y=y^x;
+      x=x^y;
+   }
+   printf("%d %d",x,y);
+    return 0;
+}
