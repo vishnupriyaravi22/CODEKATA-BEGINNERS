5bfc7bf2d48ae49fd03396713e7c42063da05e72
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-18 10:47:49 -0700

    Create 62 string binary.c

---
+++ BEG_SET_7/62 string binary.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) 
+{
+	char a[100];
+	int n,i,num,count=0;
+	scanf("%s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]=='0'||a[i]=='1')
+	    {   
+	        count++;
+	    }
+	    else
+	    {
+	       printf("no");
+	       break;
+	    }
+	}
+	if(n==count)
+	{
+	     printf("yes");
+	}
+	return 0;
+}
