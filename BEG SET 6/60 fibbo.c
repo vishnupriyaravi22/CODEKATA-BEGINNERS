c8b9bc3fc82ea7a8d21cfd4565f163660af1c72a
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-19 08:58:10 -0700

    Update 60 fibbo.c

--- BEG SET 6/60 fibbo.c
+++ BEG SET 6/60 fibbo.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int i, n, t1 = 0, t2 = 1, nextTerm;
+    int i, num, t1 = 0, t2 = 1, nextTerm;
 
-    scanf("%d", &n);
+    scanf("%d", &num);
 
 
-    for (i = 1; i <= n; ++i)
+    for (i = 1; i <= num; ++i)
     {
         printf("%d, ", t1);
         nextTerm = t1 + t2;
