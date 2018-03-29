1677e97573f8571317f2f561c2c159cb585bf532
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-29 05:36:40 -0700

    Create 107 A*B .C

---
+++ BEG SET 11/107 A*B .C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+	int n,m,ktot;
+	printf("\nEnter the three numbers : ");
+	scanf("%d",&n);
+	scanf("%d",&m);
+	scanf("%d",&k);
+	tot=(n*m)/k;
+	printf("%d",tot);
+return 0;
+}
