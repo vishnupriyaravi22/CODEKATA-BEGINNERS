88fcc6ff65530642ebb71262aa8cf93a050f2a8b
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-29 05:26:57 -0700

    Create 101 string count.c

---
+++ BEG SET 11/101 string count.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{ 
+  char a[100];
+	int n,l,i,ct=0;
+	scanf("%s",a);
+	scanf("%d",&l);
+	n=strlen(a);
+	for(i=n-1;i>=0;i--)
+	{
+	    printf("%c",a[i]);
+	    count++;
+	    if(l==c)
+	    {
+	        break;
+	    }
+	    
+	}
+	return 0;
+}
