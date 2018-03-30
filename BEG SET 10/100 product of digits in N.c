1471a1a7a03d994e0f1372562d58e1baac17c3a3
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-30 05:09:00 -0700

    Update 100 product of digits in N.c

--- BEG SET 10/100 product of digits in N.c
+++ BEG SET 10/100 product of digits in N.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void main()
 {
-    int n,res=1,r=0;
+    int n,result=1,r=0;
     scanf("%d",&n);
     while(n!='\0')
     {
         r=n%10;
-        res=res*r;
+        result=result*r;
         n=n/10;
     }
-    printf("%d",res);
+    printf("%d",result);
 }
