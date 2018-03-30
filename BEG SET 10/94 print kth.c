bfc6a06fd97378018e492ca8b91be9babedc5eed
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-30 05:16:57 -0700

    Update 94 print kth.c

--- BEG SET 10/94 print kth.c
+++ BEG SET 10/94 print kth.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 int main()
 {
-	int n,k,i,a[10];
-	scanf("%d",&n);
-	scanf("%d",&k);
-	for(i=0;i<n;i++)
+	int num,k,i,a[10];
+	scanf("%d%d",&num,&k);
+	for(i=0;i<num;i++)
 	{
 	    scanf("%d",&a[i]);
 	}
