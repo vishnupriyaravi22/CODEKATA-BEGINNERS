f75ac2932c78a0a765b4bc656a2b6e3c10e97be8
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-30 05:09:56 -0700

    Update 92 sum.c

--- BEG SET 10/92 sum.c
+++ BEG SET 10/92 sum.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	int n,a[n],i,sum=0;
+	int n,arr[n],i,sum=0;
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
-	    scanf("%d",&a);
-	    sum=sum+a;
+	    scanf("%d",&arr);
+	    sum=sum+arr;
 	}
 	printf("%d",sum);
 	return 0;
