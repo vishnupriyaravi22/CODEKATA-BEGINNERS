16fd840d9bd6fe20d16c60daa4490cb799a16015
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-30 05:28:27 -0700

    Update 97 reverse.c

--- BEG SET 10/97 reverse.c
+++ BEG SET 10/97 reverse.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void main()
 {
-    int n,rev=0,rem=0;
+    int n,reverse=0,rem=0;
     scanf("%d",&n);
     while(n!='\0')
     {
         rem=n%10;
-        rev=(rev*10)+rem;
+        reverse=(reverse*10)+rem;
         n=n/10;
     }
     printf("%d",rev);
