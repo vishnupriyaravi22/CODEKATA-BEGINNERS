d675c49deae2c265fe1c264dea5a529c524a55d0
vishnupriya <34645232+vishnupriyaravi22@users.noreply.github.com>
2018-03-30 05:09:36 -0700

    Update 91 tsa.C

--- BEG SET 10/91 tsa.C
+++ BEG SET 10/91 tsa.C
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-	int l,b,h,tsa,volume;
+	int l,b,h,tsa,vol;
 	scanf("%d %d %d",&l,&b,&h);
-	volume=l*b*h;
+	vol=l*b*h;
 	tsa=2*((l*b)+(b*h)+(h*l));
-	printf("volume=%d\n",volume);
+	printf("volume=%d\n",vol);
 	printf("TSA=%d",tsa);
 	return 0;
 }
